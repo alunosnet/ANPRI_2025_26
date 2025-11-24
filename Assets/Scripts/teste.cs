424ad6d5888bccfa4649bf5331c6e04833f5df71
User <alunosnet@gmail.com>
2025-11-24 21:31:32 +0000

    Projeto iniciado

---
+++ Assets/Scripts/teste.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class teste : MonoBehaviour
+{
+    [SerializeField] string nome="Joaquim";
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log(nome);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+}
